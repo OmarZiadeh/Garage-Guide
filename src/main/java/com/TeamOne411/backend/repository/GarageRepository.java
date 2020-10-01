11ca2d83e8b47855815ea2da265d05615dde95dd
Luke Westfall <westfalll@HarringtonHoists.com>
2020-09-30 20:18:05 -0400

    Start repository implementations

---
+++ src/main/java/com/TeamOne411/backend/repository/GarageRepository.java
@@ -0,0 +1,7 @@
+package com.TeamOne411.backend.repository;
+
+import com.TeamOne411.backend.entity.Garage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GarageRepository extends JpaRepository<Garage, Long> {
+}
