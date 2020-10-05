9fa638d20604439e893d89167857ffdc57120d10
Luke Westfall <ljw5117@psu.edu>
2020-10-05 19:48:51 -0400

    Create login page

---
+++ src/main/java/com/TeamOne411/ui/view/login/LoginView.java
@@ -0,0 +1,39 @@
+package com.TeamOne411.ui.view.login;
+
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.login.LoginForm;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.BeforeEnterEvent;
+import com.vaadin.flow.router.BeforeEnterObserver;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+
+@Route("login")
+@PageTitle("Garage Guide - Login")
+
+public class LoginView extends VerticalLayout implements BeforeEnterObserver {
+
+    private LoginForm login = new LoginForm();
+
+    public LoginView(){
+        addClassName("login-view");
+        setSizeFull();
+        setAlignItems(Alignment.CENTER);
+        setJustifyContentMode(JustifyContentMode.CENTER);
+
+        login.setAction("login");
+
+        add(new H1("Garage Guide"), login);
+    }
+
+    @Override
+    public void beforeEnter(BeforeEnterEvent beforeEnterEvent) {
+        // inform the user about an authentication error
+        if(beforeEnterEvent.getLocation()
+                .getQueryParameters()
+                .getParameters()
+                .containsKey("error")) {
+            login.setError(true);
+        }
+    }
+}
