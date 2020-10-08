247b48f21743a3a911da49a34ca4c8c69aa33554
Luke Westfall <ljw5117@psu.edu>
2020-10-07 20:26:55 -0400

    Set up Spring Security vis-a-vis CRM tutorial
    Needs a lot more work!

---
+++ src/main/java/com/TeamOne411/security/CustomRequestCache.java
@@ -0,0 +1,17 @@
+package com.TeamOne411.security;
+
+import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+class CustomRequestCache extends HttpSessionRequestCache {
+
+    @Override
+    public void saveRequest(HttpServletRequest request, HttpServletResponse response) {
+        if (!SecurityUtils.isFrameworkInternalRequest(request)) {
+            super.saveRequest(request, response);
+        }
+    }
+
+}
--- src/main/java/com/TeamOne411/ui/MainLayout.java
+++ src/main/java/com/TeamOne411/ui/MainLayout.java
@@ -4,6 +4,10 @@ import com.TeamOne411.ui.view.PlaceholderHomeView;
 import com.TeamOne411.ui.view.login.LoginView;
 import com.TeamOne411.ui.view.sandbox.SandboxView;
 import com.vaadin.flow.component.applayout.AppLayout;
+import com.vaadin.flow.component.html.Anchor;
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.orderedlayout.FlexComponent;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.RouterLink;
 import com.vaadin.flow.server.PWA;
@@ -14,6 +18,7 @@ import com.vaadin.flow.server.PWA;
 @PWA(name = "GarageGuide", shortName = "GarageGuide", enableInstallPrompt = false)
 public class MainLayout extends AppLayout {
     public MainLayout() {
+        createHeader();
         createDrawer();
     }
 
@@ -23,5 +28,22 @@ public class MainLayout extends AppLayout {
                 new RouterLink("Sandbox", SandboxView.class),
                 new RouterLink("Login", LoginView.class)
         ));
+    }
+
+    private void createHeader() {
+        H1 logo = new H1("Garage Guide");
+        logo.addClassName("logo");
+
+        Anchor logout = new Anchor("logout", "Log out");
+
+        HorizontalLayout header = new HorizontalLayout(logo, logout);
+        header.expand(logo);
+        header.setDefaultVerticalComponentAlignment(FlexComponent.Alignment.CENTER);
+        header.setWidth("100%");
+        header.addClassName("header");
+
+        header.setPadding(true);
+
+        addToNavbar(header);
     }
 }
