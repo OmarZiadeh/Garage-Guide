79d13b3f7003b3729d439ff1ad2b9c8502d72939
Luke Westfall <ljw5117@psu.edu>
2020-10-10 12:25:19 -0400

    Fix repository method conventions and remove query override

--- src/main/java/com/TeamOne411/backend/repository/UserAccountRepository.java
+++ src/main/java/com/TeamOne411/backend/repository/UserAccountRepository.java
@@ -1,11 +1,11 @@
 package com.TeamOne411.backend.repository;
 
 import com.TeamOne411.backend.entity.users.UserAccount;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 
 public interface UserAccountRepository extends CrudRepository<UserAccount, Long> {
-    @Query(value = "SELECT u.user_name FROM user_account u WHERE u.user_name = :userName", nativeQuery = true)
-    public UserAccount getUserByUsername(@Param("userName") String userName);
+    public UserAccount findByUserName(@Param("userName") String userName);
+
+    public UserAccount findByEmailAddress(@Param("emailAddress") String emailAddress);
 }
