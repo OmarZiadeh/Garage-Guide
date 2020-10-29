42c0f01c03a38bec4928f0bdde881e8e56d6be4f
Elizabeth Kelly <eak5496@psu.edu>
2020-10-29 10:49:43 -0700

    Moved default list to ServiceCatalogService

--- src/main/java/com/TeamOne411/backend/config/SetupDataLoader.java
+++ src/main/java/com/TeamOne411/backend/config/SetupDataLoader.java
@@ -35,12 +35,6 @@ public class SetupDataLoader implements
     @Autowired
     private PasswordEncoder passwordEncoder;
 
-    @Autowired
-    private OfferedServiceRepository offeredServiceRepository;
-
-    @Autowired
-    private ServiceCategoryRepository serviceCategoryRepository;
-
     @Override
     @Transactional
     public void onApplicationEvent(ContextRefreshedEvent event) {
@@ -61,80 +55,7 @@ public class SetupDataLoader implements
 
         createTestUserIfNotFound();
 
-        //initializeDefaultServiceCatalog();
-
         alreadySetup = true;
-    }
-
-    public void initializeDefaultServiceCatalog() {
-        //These are the default Categories that are enabled for a garage
-        ServiceCategory catRoutineMaintenance = createServiceCategoryIfNotFound("Routine Maintenance");
-        ServiceCategory catTires = createServiceCategoryIfNotFound("Tires");
-        ServiceCategory catBatteries = createServiceCategoryIfNotFound("Batteries");
-        ServiceCategory catShocksStruts = createServiceCategoryIfNotFound("Shocks & Struts");
-        ServiceCategory catOther = createServiceCategoryIfNotFound("Other/Not Sure");
-
-        //Routine Maintenance
-        createOfferedServiceIfNotFound("Oil Change", catRoutineMaintenance);
-        createOfferedServiceIfNotFound("Brake Replacement", catRoutineMaintenance);
-        createOfferedServiceIfNotFound("Headlight Replacement", catRoutineMaintenance);
-        createOfferedServiceIfNotFound("Wiper Blade Replacement", catRoutineMaintenance);
-        createOfferedServiceIfNotFound("Power Steering and Suspension", catRoutineMaintenance);
-        createOfferedServiceIfNotFound("Check: Fluids", catRoutineMaintenance);
-        createOfferedServiceIfNotFound("Check: Brakes", catRoutineMaintenance);
-        createOfferedServiceIfNotFound("Check: Belts & Hoses", catRoutineMaintenance);
-        createOfferedServiceIfNotFound("Check: Vehicle Health", catRoutineMaintenance);
-        createOfferedServiceIfNotFound("Check: Air & Cabin Filters", catRoutineMaintenance);
-        createOfferedServiceIfNotFound("Check: Alternators & Starters", catRoutineMaintenance);
-
-        //Tires
-        createOfferedServiceIfNotFound("New Tires", catTires);
-        createOfferedServiceIfNotFound("Alignment", catTires);
-        createOfferedServiceIfNotFound("Flat Repair", catTires);
-        createOfferedServiceIfNotFound("TPMS Service", catTires);
-        createOfferedServiceIfNotFound("Wheel Balance", catTires);
-        createOfferedServiceIfNotFound("Tire Rotation", catTires);
-        createOfferedServiceIfNotFound("Seasonal Changeover", catTires);
-        createOfferedServiceIfNotFound("Check: Pre-Trip Safety", catTires);
-
-        //Batteries
-        createOfferedServiceIfNotFound("Battery Check", catBatteries);
-        createOfferedServiceIfNotFound("Battery Installation", catBatteries);
-
-        //Shocks & Struts
-        createOfferedServiceIfNotFound("Struts & Shocks Consultation", catShocksStruts);
-        //consultation differs from check in restoration/upgrade preference vs current safety status - Hope
-        createOfferedServiceIfNotFound("Check: Struts & Suspension", catShocksStruts);
-
-        //Other/Not Sure - no services to include, but does need to a descriptor for car owner clarity
-        createOfferedServiceIfNotFound("Other/Not Sure", catOther);
-    }
-
-    @Transactional
-    OfferedService createOfferedServiceIfNotFound(String name, ServiceCategory category) {
-        OfferedService service = offeredServiceRepository.findByServiceName(name);
-
-        if (service == null) {
-            service = new OfferedService();
-            service.setServiceName(name);
-            service.setServiceCategory(category);
-            offeredServiceRepository.save(service);
-        }
-
-        return service;
-    }
-
-    @Transactional
-    ServiceCategory createServiceCategoryIfNotFound(String name) {
-        ServiceCategory category = serviceCategoryRepository.findByCategoryName(name);
-
-        if (category == null) {
-            category = new ServiceCategory();
-            category.setCategoryName(name);
-            serviceCategoryRepository.save(category);
-        }
-
-        return category;
     }
 
     private void createTestUserIfNotFound() {
--- src/main/java/com/TeamOne411/backend/service/ServiceCatalogService.java
+++ src/main/java/com/TeamOne411/backend/service/ServiceCatalogService.java
@@ -62,4 +62,74 @@ public class ServiceCatalogService {
     public void deleteServiceCategory(ServiceCategory serviceCategory) {
         serviceCategoryRepository.delete(serviceCategory);
     }
+
+    public void initializeDefaultServiceCatalog(Garage garage) {
+        //These are the default Categories that are enabled for a garage
+        ServiceCategory catRoutineMaintenance = createDefaultServiceCategory("Routine Maintenance", garage);
+        ServiceCategory catTires = createDefaultServiceCategory("Tires", garage);
+        ServiceCategory catBatteries = createDefaultServiceCategory("Batteries", garage);
+        ServiceCategory catShocksStruts = createDefaultServiceCategory("Shocks & Struts", garage);
+        ServiceCategory catOther = createDefaultServiceCategory("Other/Not Sure", garage);
+
+        //Routine Maintenance
+        createDefaultOfferedService("Oil Change", catRoutineMaintenance);
+        createDefaultOfferedService("Brake Replacement", catRoutineMaintenance);
+        createDefaultOfferedService("Headlight Replacement", catRoutineMaintenance);
+        createDefaultOfferedService("Wiper Blade Replacement", catRoutineMaintenance);
+        createDefaultOfferedService("Power Steering and Suspension", catRoutineMaintenance);
+        createDefaultOfferedService("Check: Fluids", catRoutineMaintenance);
+        createDefaultOfferedService("Check: Brakes", catRoutineMaintenance);
+        createDefaultOfferedService("Check: Belts & Hoses", catRoutineMaintenance);
+        createDefaultOfferedService("Check: Vehicle Health", catRoutineMaintenance);
+        createDefaultOfferedService("Check: Air & Cabin Filters", catRoutineMaintenance);
+        createDefaultOfferedService("Check: Alternators & Starters", catRoutineMaintenance);
+
+        //Tires
+        createDefaultOfferedService("New Tires", catTires);
+        createDefaultOfferedService("Alignment", catTires);
+        createDefaultOfferedService("Flat Repair", catTires);
+        createDefaultOfferedService("TPMS Service", catTires);
+        createDefaultOfferedService("Wheel Balance", catTires);
+        createDefaultOfferedService("Tire Rotation", catTires);
+        createDefaultOfferedService("Seasonal Changeover", catTires);
+        createDefaultOfferedService("Check: Pre-Trip Safety", catTires);
+
+        //Batteries
+        createDefaultOfferedService("Battery Check", catBatteries);
+        createDefaultOfferedService("Battery Installation", catBatteries);
+
+        //Shocks & Struts
+        createDefaultOfferedService("Struts & Shocks Consultation", catShocksStruts);
+        //consultation differs from check in restoration/upgrade preference vs current safety status - Hope
+        createDefaultOfferedService("Check: Struts & Suspension", catShocksStruts);
+
+        //Other/Not Sure - no services to include, but does need to a descriptor for car owner clarity
+        createDefaultOfferedService("Other/Not Sure", catOther);
+    }
+
+    ServiceCategory createDefaultServiceCategory(String name, Garage garage) {
+        ServiceCategory category = serviceCategoryRepository.findByCategoryName(name);
+
+        if (category == null) {
+            category = new ServiceCategory();
+            category.setCategoryName(name);
+            category.setGarage(garage);
+            serviceCategoryRepository.save(category);
+        }
+        return category;
+    }
+
+    OfferedService createDefaultOfferedService(String name, ServiceCategory category) {
+        OfferedService service = offeredServiceRepository.findByServiceName(name);
+
+        if (service == null) {
+            service = new OfferedService();
+            service.setServiceName(name);
+            service.setServiceCategory(category);
+            offeredServiceRepository.save(service);
+        }
+
+        return service;
+    }
+
 }
