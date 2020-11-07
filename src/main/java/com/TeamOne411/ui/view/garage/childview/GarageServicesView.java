5817fae33056b944c62e895a653cffbc99311791
Elizabeth Kelly <eak5496@psu.edu>
2020-11-07 14:58:17 -0800

    Building out the GarageServicesView

--- src/main/java/com/TeamOne411/ui/view/garage/childview/GarageServicesView.java
+++ src/main/java/com/TeamOne411/ui/view/garage/childview/GarageServicesView.java
@@ -1,11 +1,26 @@
 package com.TeamOne411.ui.view.garage.childview;
 
 
-import com.vaadin.flow.component.html.H3;
+import com.TeamOne411.backend.entity.Garage;
+import com.TeamOne411.backend.entity.servicecatalog.OfferedService;
+import com.TeamOne411.backend.service.ServiceCatalogService;
+import com.TeamOne411.ui.view.garage.form.CategoryEditorDialog;
+import com.TeamOne411.ui.view.garage.form.ServiceEditorDialog;
+import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 
 public class GarageServicesView extends VerticalLayout {
-    public GarageServicesView() {
-        add(new H3("Services Configuration - Coming soon..."));
-    }
+    ServiceCatalogService serviceCatalogService;
+
+    private Grid<OfferedService> grid = new Grid<>(OfferedService.class);
+    private Garage garage;
+    private CategoryEditorDialog categoryEditorDialog;
+    private ServiceEditorDialog serviceEditorDialog;
+
+
+
+
+
+
+
 }
